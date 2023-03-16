85f7a7b05d8eaa063368037169719f809ba09a6b
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-16 10:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  if (input_digit.length === 0) {
+    return [];
 }
-
+const mapping = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz"
+  };
+  
+  const result = [];
+  
+  function backtrack(currentString, digits) {
+    if (digits.length === 0) {
+      result.push(currentString);
+      return;
+    }
+    const letters = mapping[digits[0]];
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(currentString + letter, digits.slice(1));
+    }
+  }
+  
+  backtrack("", digits);
+  return result;
+}
 module.exports = letterCombinations;
